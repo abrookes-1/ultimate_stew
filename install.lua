aca2cd338fb1f9f9f24b11c53373a0781aed7b43
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 10:39:20 -0600

    a

--- install.lua
+++ install.lua
@@ -1,13 +1,9 @@
 local mainURL = "https://raw.githubusercontent.com/abrookes-1/ultimate_stew/master/main.lua"
 local food_namesURL = "https://raw.githubusercontent.com/abrookes-1/ultimate_stew/master/food_names.csv"
 local functions = "https://raw.githubusercontent.com/abrookes-1/ultimate_stew/master/lib/f.lua"
-
-urls = {
-    "stew": mainURL,
-    "food_names.csv": food_namesURL,
-    "lib/f": functions
-}
-
+ 
+urls = {["stew"]=mainURL, ["food_names.csv"]=food_namesURL, ["lib/f"]=functions}
+ 
 for path, url in pairs(urls) do
     ret = http.get(url)
     contents = ret.readAll()
