4c044e344abf98e9a0bd33b3c2eb2a7f467df722
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 07:06:48 -0600

    a

--- main.lua
+++ main.lua
@@ -1,10 +1,18 @@
 -- set the location of your me block relative to the computer
-local meSide = "appeng_me_tileterminal_0"
+local meSide = "back"
+local chestSide = "top"
+--local meSide = "appeng_me_tileterminal_0"
 
 local me = peripheral.wrap(meSide)
+local chest = peripheral.wrap(chestSide)
 
 if me == null then
-    error("me block not found")
+    error("me interface not found")
 end
 
 
+for k, item in pairs(chest.getAllStacks())do
+    ae_info = me.getItemDetail(item.basic())
+    amount = ae_info.basic().qty
+    print(amount)
+end
