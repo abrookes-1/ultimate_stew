7f59cc76135f0e51325945846682bc23ace5625a
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 09:40:17 -0600

    a

--- main.lua
+++ main.lua
@@ -1,24 +1,34 @@
 -- set the location of your me block relative to the computer
 local meSide = "back"
-local food_names_file = "food_names.csv"
---local meSide = "appeng_me_tileterminal_0"
+local foodNamesPath = "food_names.csv"
 
-os.loadAPI("csvConverter")
-csvConverter.write(food_names, food_names_file)
-
+-- get me system
 local me = peripheral.wrap(meSide)
-
 if me == null then
     error("me interface not found")
 end
 
 items = me.listAvailableItems()
 
-for indx, stack in pairs(items) do
-    --print(stack["name"])
-    --print(stack["count"]
+-- for indx, stack in pairs(items) do
+--     --print(stack["name"])
+--     --print(stack["count"]
+-- end
+
+-- read csv with item names
+local foodNames{}
+local foodNamesFile = fs.open(foodNamesPath, "r")
+local line = foodNamesFile.readLine()
+
+while line do
+    table.insert(foodNames, line)
+    line = f.readLine()
 end
 
+foodNamesFile.close()
+
 print(food_names[1][1])
+
+
 
 print("done")
