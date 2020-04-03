e081a22467b61c5fe6b2cc65b684eada8c0c3eb4
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 10:28:32 -0600

    a

--- install.lua
+++ install.lua
@@ -1,20 +1,17 @@
 local mainURL = "https://raw.githubusercontent.com/abrookes-1/ultimate_stew/master/main.lua"
 local food_namesURL = "https://raw.githubusercontent.com/abrookes-1/ultimate_stew/master/food_names.csv"
+local functions = "https://raw.githubusercontent.com/abrookes-1/ultimate_stew/master/lib/f.lua"
 
-local main
+urls = {
+    "stew": mainURL,
+    "food_names.csv": food_namesURL,
+    "lib/f": functions
+}
 
--- open main and write to file system
-main = http.get(mainURL)
-mainFile = main.readAll()
-
-local file1 = fs.open("stew", "w")
-file1.write(mainFile)
-file1.close()
-
--- open csv and write to file system
-food_names = http.get(food_namesURL)
-food_namesFile = food_names.readAll()
-
-local file2 = fs.open("food_names.csv", "w")
-file2.write(food_namesFile)
-file2.close()
+for path, url in pairs(urls) do
+    ret = http.get(url)
+    contents = ret.readAll()
+    file = fs.open(path, "w")
+    file.write(contents)
+    file.close()
+end
