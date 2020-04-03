bcaf8f6e5812b0a2c15e3ebfd14201e0165cf28b
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 10:51:54 -0600

    a

--- lib/f.lua
+++ lib/f.lua
@@ -9,4 +9,22 @@ function periphSearch(type)
     return null
  end
 
+ --display text text on monitor, "mon" peripheral
+function draw_text(mon, x, y, text, text_color, bg_color)
+   mon.monitor.setBackgroundColor(bg_color)
+   mon.monitor.setTextColor(text_color)
+   mon.monitor.setCursorPos(x,y)
+   mon.monitor.write(text)
+ end
  
+ function draw_text_right(mon, offset, y, text, text_color, bg_color)
+   mon.monitor.setBackgroundColor(bg_color)
+   mon.monitor.setTextColor(text_color)
+   mon.monitor.setCursorPos(mon.X-string.len(tostring(text))-offset,y)
+   mon.monitor.write(text)
+ end
+ 
+ function draw_text_lr(mon, x, y, offset, text1, text2, text1_color, text2_color, bg_color)
+    draw_text(mon, x, y, text1, text1_color, bg_color)
+    draw_text_right(mon, offset, y, text2, text2_color, bg_color)
+ end
