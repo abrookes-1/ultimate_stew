65598ca0b3feb8a178f673ce6ebfcb499a7b0c27
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 09:31:46 -0600

    a

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 -- set the location of your me block relative to the computer
 local meSide = "back"
+local food_names_file = "food_names.csv"
 --local meSide = "appeng_me_tileterminal_0"
+
+os.loadAPI("csvConverter")
+csvConverter.write(food_names, food_names_file)
 
 local me = peripheral.wrap(meSide)
 
@@ -11,9 +15,10 @@ end
 items = me.listAvailableItems()
 
 for indx, stack in pairs(items) do
-    print(stack["name"])
-    print(stack["count"]
+    --print(stack["name"])
+    --print(stack["count"]
 end
 
+print(food_names[1][1])
 
 print("done")
