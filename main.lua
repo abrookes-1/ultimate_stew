ef4cb26df56dea585e8b9b5863a2663924d74b64
abrookes-1 <aaabrookes@gmail.com>
2020-04-03 10:51:49 -0600

    a

--- main.lua
+++ main.lua
@@ -9,6 +9,14 @@ os.loadAPI("lib/f")
 local mon, monitor, monX, monY
 monitor = f.periphSearch("monitor")
 
+if monitor == null then
+	error("No valid monitor was found")
+end
+
+mon = {}
+monX, monY = monitor.getSize()
+mon.monitor, mon.X, mon.Y = monitor, monX, monY
+
 -- get me system
 local me = peripheral.wrap(meSide)
 if me == null then
@@ -27,8 +35,25 @@ end
 
 foodNamesFile.close()
 
+-- compare required items with me inventory
+local currentInventory = {}
+
+for k, id in pairs(foodNames) do
+    searchResult = me.findItems(id)
+    if searchResult[1] then
+        meta = searchResult[1].getMetaData()
+        currentInventory[id] = meta.count
+    else
+        currentInventory[id] = 0
+    end
+end
 
 
 
+
+-- print to monitor
+for id, count in pairs(currentInventory) do
+    f.draw_text_lr(mon, 2, 19, 1, id, count, colors.gray, colors.gray, colors.black)
+end
 
 print("done")
